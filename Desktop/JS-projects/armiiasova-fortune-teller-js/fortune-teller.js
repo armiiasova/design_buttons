ffbf27a05c924bd74de2ba54f91d27cf1dcd5227
Cholpon Zhanybek uulu <armiiasova.pl@gmail.com>
2023-02-26 20:17:17 +0100

    My changes

---
+++ Desktop/JS-projects/armiiasova-fortune-teller-js/fortune-teller.js
@@ -0,0 +1,5 @@
+const numChildren =3;
+const partner = "Ryan Reynolds";
+const loc = "Los Angeles,CA";
+const jobTitle = "Actress";
+console.log( "You will be a " + jobTitle + " in " + loc + " and married to " + " with " + numChildren + " kids ");
